ba992e67cb7f6c05f4666e7db199451b9b2f98a3
Thomas Perkov <thomas@perkov.dk>
2017-09-11 12:49:37 +0200

    Project structure files etc.

---
+++ delphin_6_automation.py
@@ -0,0 +1,6 @@
+def main():
+	print(42)
+
+if __name__ == "__main__":
+    main()
+
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='delphin_6_automation',
+      version='00.01',
+      description='Automation script for the hygrothermal simulation program Delphin 6 made under the EU research project, RIBuild.',
+      url='https://github.com/thp44/delphin_6_automation.git',
+      author='Thomas Perkov',
+      author_email='thomas@perkov.dk',
+      license='MIT',
+      packages=[''],
+      zip_safe=False)
