1341072c4b7658a94dc996dd66cc57532c832a82
ocni <ocni@dtu.dk>
2017-11-27 12:47:13 +0100

    added database collections

---
+++ delphin_6_automation/nosql/database_collections.py
@@ -0,0 +1,27 @@
+__author__ = "Christian Kongsgaard"
+__license__ = "MIT"
+__version__ = "0.0.1"
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# IMPORTS
+
+# Modules:
+
+
+# RiBuild Modules:
+
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# DATABASE COLLECTIONS
+
+material_db = {'db_alias': 'core',
+               'collection': 'material'}
+
+weather_db = {'db_alias': 'core',
+              'collection': 'weather'}
+
+result_db = {'db_alias': 'core',
+             'collection': 'results'}
+
+delphin_db = {'db_alias': 'core',
+              'collection': 'delphin'}
