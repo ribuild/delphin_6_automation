0999835308bcd83e959d350025cd270e2a581a03
ocni <ocni@dtu.dk>
2017-12-04 10:52:17 +0100

    fixed db_alias

--- delphin_6_automation/nosql/database_collections.py
+++ delphin_6_automation/nosql/database_collections.py
@@ -14,14 +14,14 @@ __version__ = "0.0.1"
 # -------------------------------------------------------------------------------------------------------------------- #
 # DATABASE COLLECTIONS
 
-material_db = {'db_alias': 'core',
+material_db = {'db_alias': 'local',
                'collection': 'material'}
 
-weather_db = {'db_alias': 'core',
+weather_db = {'db_alias': 'local',
               'collection': 'weather'}
 
-result_db = {'db_alias': 'core',
+result_db = {'db_alias': 'local',
              'collection': 'results'}
 
-delphin_db = {'db_alias': 'core',
+delphin_db = {'db_alias': 'local',
               'collection': 'delphin'}
