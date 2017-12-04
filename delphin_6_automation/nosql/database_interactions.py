3a0ef66f8e7788cafd2044b3ac434d1a821064b0
ocni <ocni@dtu.dk>
2017-12-04 10:54:36 +0100

    added doc string and type hint

--- delphin_6_automation/nosql/database_interactions.py
+++ delphin_6_automation/nosql/database_interactions.py
@@ -15,8 +15,15 @@ from delphin_6_automation.nosql.db_templates.delphin_entry import Delphin
 # DATABASE INTERACTIONS
 
 
-def gather_material_list(delphin_id):
+def gather_material_list(delphin_id: str)->list:
+    """
+    Gathers the material file names of Delphin file in the database
+    :param delphin_id: database id
+    :return: list of material file names
+    """
+
     delphin_document = Delphin.objects(id=delphin_id).first()
+
     material_list = []
     for material_dict in delphin_document['dp6_file']['DelphinProject']['Materials']['MaterialReference']:
         material_list.append(material_dict['#text'].split('/')[-1])
--- test/read_dp6.py
+++
@@ -1,7 +0,0 @@
-import delphin_6_automation.delphin_db as de
-
-folder_path = r'C:\Users\ocni\PycharmProjects\delphin_6_automation\dephin_6_templates'
-template0 = 'template_0.d6p'
-
-a = de.dp6_to_dict(folder_path + '/' + template0)
-print(a)
