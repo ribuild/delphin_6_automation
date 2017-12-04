549d29885136ea6c138642e6f2832fcca65c3a89
ocni <ocni@dtu.dk>
2017-12-04 16:37:56 +0100

    update to solve_delphin

--- delphin_6_automation/delphin.py
+++ delphin_6_automation/delphin.py
@@ -6,7 +6,8 @@ __version__ = "0.0.1"
 # IMPORTS
 
 # Modules:
-import asyncio.subprocess as subprocess
+#import asyncio.subprocess as subprocess
+import subprocess
 
 # RiBuild Modules:
 
@@ -14,9 +15,12 @@ import asyncio.subprocess as subprocess
 # DELPHIN FUNCTIONS AND CLASSES
 
 
-def solve_delphin(file, delphin_exe=r'C:/Program Files/IBK/Delphin 6.0/DelphinSolverUI.exe', verbosity_level=1):
+def solve_delphin(file, delphin_exe=r'C:/Program Files/IBK/Delphin 6.0/DelphinSolver.exe', verbosity_level=1):
     """Solves a delphin file"""
 
-    verbosity = "verbosity-level = "+str(verbosity_level)
-    retcode = subprocess.Process([delphin_exe, verbosity, file])
+    verbosity = "verbosity-level=" + str(verbosity_level)
+    command_string = '"' + str(delphin_exe) + '" --close-on-exit --' + verbosity + ' "' + file + '"'
+    #print(command_string)
+    retcode = subprocess.run(command_string, shell=True)
+
     return retcode
