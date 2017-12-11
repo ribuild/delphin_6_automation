6c0c40fa8dac3d2bc3f625d50c45d6796dffa9f5
ocni <ocni@dtu.dk>
2017-12-11 10:31:06 +0100

    added download_raw_results

--- delphin_6_automation/nosql/database_interactions.py
+++ delphin_6_automation/nosql/database_interactions.py
@@ -9,7 +9,8 @@ __version__ = "0.0.1"
 
 
 # RiBuild Modules:
-from delphin_6_automation.nosql.db_templates.delphin_entry import Delphin
+from delphin_6_automation.nosql.db_templates.delphin_entry import delphin_db
+from delphin_6_automation.nosql.db_templates.result_entry import result_db
 
 # -------------------------------------------------------------------------------------------------------------------- #
 # DATABASE INTERACTIONS
@@ -22,10 +23,24 @@ def gather_material_list(delphin_id: str)->list:
     :return: list of material file names
     """
 
-    delphin_document = Delphin.objects(id=delphin_id).first()
+    delphin_document = delphin_db.objects(id=delphin_id).first()
 
     material_list = []
     for material_dict in delphin_document['dp6_file']['DelphinProject']['Materials']['MaterialReference']:
         material_list.append(material_dict['#text'].split('/')[-1])
 
     return material_list
+
+
+def download_raw_result(result_id, download_path):
+    result_obj = result_db.objects(id=result_id).first()
+
+    delphin_interact.write_log_files(result_obj, download_path)
+    delphin_interact.write_result_files(result_obj, download_path)
+    delphin_interact.write_geometry_files(result_obj, download_path)
+
+    return True
+
+
+def download_result(result_id, download_path):
+    return None
