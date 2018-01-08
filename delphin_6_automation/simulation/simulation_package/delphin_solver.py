8a84865ef5b96470dc6fb4f68c2e2b6f8af50c23
ocni <ocni@dtu.dk>
2018-01-08 12:20:19 +0100

    update to solve_delphin

--- delphin_6_automation/simulation/simulation_package/delphin_solver.py
+++ delphin_6_automation/simulation/simulation_package/delphin_solver.py
@@ -20,6 +20,5 @@ def solve_delphin(file, delphin_exe=r'C:/Program Files/IBK/Delphin 6.0/DelphinSo
 
     verbosity = "verbosity-level=" + str(verbosity_level)
     command_string = '"' + str(delphin_exe) + '" --close-on-exit --' + verbosity + ' "' + file + '"'
-    retcode = subprocess.run(command_string, shell=True)
 
-    return retcode
+    return subprocess.run(command_string, shell=True)
