21030b99826dc611dd7189f5acce96eb5be15b2d
ocni <ocni@dtu.dk>
2018-01-09 15:36:55 +0100

    updated tests

---
+++ delphin_6_automation/pytest/test_download_simulate_upload.py
@@ -0,0 +1,53 @@
+__author__ = "Christian Kongsgaard"
+
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# IMPORTS
+
+# Modules:
+
+
+# RiBuild Modules:
+import delphin_6_automation.simulation.database_interactions.delphin_interactions as delphin_interact
+import delphin_6_automation.simulation.nosql.mongo_setup as mongo_setup
+from delphin_6_automation.simulation.nosql.auth import dtu_byg
+import delphin_6_automation.simulation.nosql.db_templates.result_entry as result_db
+import delphin_6_automation.pytest.pytest_helper_functions as helper
+import delphin_6_automation.simulation.simulation_package.delphin_solver as solver
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# TEST
+
+mongo_setup.global_init(dtu_byg)
+
+
+def download():
+    id_ = "5a5479095d9460327c6970f0"
+    test_path, source_path = helper.setup_test_folders()
+    delphin_interact.download_from_database(id_, test_path)
+    test_file = test_path + '/' + str(id_) + '.d6p'
+
+    return test_file, test_path
+
+
+def simulate(test_file):
+    solver.solve_delphin(test_file)
+
+
+def upload(test_folder):
+    id_ = delphin_interact.results_to_mongo_db(test_folder + '/5a5479095d9460327c6970f0')
+    test_doc = result_db.Result.objects(id=id_).first()
+
+    return test_doc
+
+
+def clean_up(test_doc):
+    helper.clean_up_test_folders()
+    test_doc.delete()
+
+
+def test_download_simulate_upload():
+    test_file, test_folder = download()
+    simulate(test_file)
+    test_doc = upload(test_folder)
+    clean_up(test_doc)
--- delphin_6_automation/pytest/test_upload_to_db.py
+++ delphin_6_automation/pytest/test_upload_to_db.py
@@ -6,12 +6,15 @@ __author__ = "Christian Kongsgaard"
 
 # Modules:
 import os
+import shutil
 
 # RiBuild Modules:
 import delphin_6_automation.simulation.database_interactions.delphin_interactions as delphin_interact
 import delphin_6_automation.simulation.nosql.mongo_setup as mongo_setup
 from delphin_6_automation.simulation.nosql.auth import dtu_byg
 import delphin_6_automation.simulation.nosql.db_templates.delphin_entry as delphin_db
+import delphin_6_automation.simulation.nosql.db_templates.result_entry as result_db
+import delphin_6_automation.pytest.pytest_helper_functions as helper
 
 # -------------------------------------------------------------------------------------------------------------------- #
 # TEST
@@ -19,15 +22,49 @@ import delphin_6_automation.simulation.nosql.db_templates.delphin_entry as delph
 mongo_setup.global_init(dtu_byg)
 
 
-def test_upload_1():
+def test_upload_project_1():
     delphin_file = os.path.dirname(os.path.realpath(__file__)) + '/test_files/5a5479095d9460327c6970f0.d6p'
     id_ = delphin_interact.upload_to_database(delphin_file, 10)
+
     test_doc = delphin_db.Delphin.objects(id=id_).first()
     test_dict = test_doc.to_mongo()
     test_dict.pop('_id')
     test_dict.pop('added_date')
+
     source_dict = delphin_db.Delphin.objects(id='5a5479095d9460327c6970f0').first().to_mongo()
     source_dict.pop('_id')
     source_dict.pop('added_date')
+
     assert test_dict == source_dict
     test_doc.delete()
+
+
+def test_upload_results_1():
+    test_path, source_path = helper.setup_test_folders()
+    result_zip = os.path.dirname(os.path.realpath(__file__)) + '/test_files/5a5479095d9460327c6970f0.zip'
+    result_folder = test_path
+    shutil.unpack_archive(result_zip, result_folder)
+    id_ = delphin_interact.results_to_mongo_db(result_folder + '/5a5479095d9460327c6970f0')
+
+    test_doc = result_db.Result.objects(id=id_).first()
+    test_dict = test_doc.to_mongo()
+    test_dict.pop('_id')
+    test_dict.pop('added_date')
+
+    source_dict = result_db.Result.objects(id='5a54a8a35d946027d804103c').first().to_mongo()
+    source_dict.pop('_id')
+    source_dict.pop('added_date')
+
+    assert test_dict == source_dict
+    test_doc.delete()
+    helper.clean_up_test_folders()
+
+
+def test_upload_materials_1():
+    # TODO
+    pass
+
+
+def test_upload_weather_1():
+    # TODO
+    pass
