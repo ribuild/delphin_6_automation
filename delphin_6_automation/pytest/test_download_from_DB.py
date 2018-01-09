ae73e0bc1d8fe417842bc6d69dee731bfa225a9d
ocni <ocni@dtu.dk>
2018-01-09 09:46:59 +0100

    updated test_download_from_db

--- delphin_6_automation/pytest/test_download_from_DB.py
+++ delphin_6_automation/pytest/test_download_from_DB.py
@@ -1,13 +1,31 @@
+__author__ = "Christian Kongsgaard"
+
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# IMPORTS
+
+# Modules:
+import filecmp
+import os
+import shutil
+
+# RiBuild Modules:
 import delphin_6_automation.simulation.database_interactions.delphin_interactions as delphin_interact
 import delphin_6_automation.simulation.nosql.mongo_setup as mongo_setup
 import delphin_6_automation.pytest.pytest_helper_functions as helper
-import filecmp
+from delphin_6_automation.simulation.nosql.auth import dtu_byg
 
-mongo_setup.global_init('test')
-test_path, source_path = helper.file_structures_setup()
-id_ = "5a25121f5d94600c683b82bd"
+# -------------------------------------------------------------------------------------------------------------------- #
+# TEST
+
+mongo_setup.global_init(dtu_byg)
 
 
-def test_download():
+def test_download_1():
+    id_ = "5a5479095d9460327c6970f0"
+    test_path, source_path = helper.setup_test_folders()
+    test_file_path = os.path.dirname(os.path.realpath(__file__)) + '/test_files'
     delphin_interact.download_from_database(id_, test_path)
+    shutil.copy(test_file_path + '/' + id_ + '.d6p', source_path + '/' + id_ + '.d6p')
     assert filecmp.cmp(source_path + '/' + str(id_) + '.d6p', test_path + '/' + str(id_) + '.d6p')
+    helper.clean_up_test_folders()
