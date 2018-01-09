c046b05826b16609a463ca481d5426015cee076e
ocni <ocni@dtu.dk>
2018-01-09 09:33:15 +0100

    updated pytest_helper_functions

--- delphin_6_automation/pytest/pytest_helper_functions.py
+++ delphin_6_automation/pytest/pytest_helper_functions.py
@@ -1,19 +1,47 @@
+__author__ = "Christian Kongsgaard"
+__license__ = "MIT"
+__version__ = "0.0.1"
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# IMPORTS
+
+# Modules:
 import os
 import shutil
 
+# RiBuild Modules:
 
-def file_structures_setup():
-    default_path = r'C:\Users\ocni\PycharmProjects\delphin_6_automation\delphin_6_automation\pytest'
-    files_dir = default_path + '/test_files'
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# TEST HELPER FUNCTIONS
+
+def setup_test_folders():
+    default_path = os.path.dirname(os.path.realpath(__file__))
+
+    test_dir = default_path + '/test_dir'
     test_dir_source = default_path + '/test_dir/source'
     test_dir_test = default_path + '/test_dir/test'
-    os.mkdir(test_dir_source)
-    os.mkdir(test_dir_test)
-    shutil.unpack_archive(files_dir + '/5a25121f5d94600c683b82bd.zip', test_dir_source)
+
+    if not os.path.isdir(test_dir):
+        os.mkdir(test_dir)
+    if not os.path.isdir(test_dir_source):
+        os.mkdir(test_dir_source)
+    if not os.path.isdir(test_dir_test):
+        os.mkdir(test_dir_test)
 
     return test_dir_test, test_dir_source
 
 
-def clean_up():
-    test_dir = r'C:\Users\ocni\PycharmProjects\delphin_6_automation\delphin_6_automation\pytest\test_dir'
+def unzip_with_test_folder_setup(zip_file):
+    test_dir_test, test_dir_source = setup_test_folders()
+
+    default_path = os.path.dirname(os.path.realpath(__file__))
+    files_dir = default_path + '/test_files'
+    shutil.unpack_archive(files_dir + '/' + zip_file, test_dir_source)
+
+    return test_dir_test, test_dir_source
+
+
+def clean_up_test_folders():
+    test_dir = os.path.dirname(os.path.realpath(__file__)) + '/test_dir'
     shutil.rmtree(test_dir)
