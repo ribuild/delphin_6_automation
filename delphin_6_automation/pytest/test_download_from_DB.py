9fc65a1c4c6ea3f4ae2f153e31cda3ee28c18c18
ocni <ocni@dtu.dk>
2018-01-09 15:31:05 +0100

    added delphin_6_automation-3 Setup PyTest

--- delphin_6_automation/pytest/test_download_from_DB.py
+++ delphin_6_automation/pytest/test_download_from_DB.py
@@ -11,9 +11,11 @@ import shutil
 
 # RiBuild Modules:
 import delphin_6_automation.simulation.database_interactions.delphin_interactions as delphin_interact
+import delphin_6_automation.simulation.database_interactions.general_interactions as general_interact
 import delphin_6_automation.simulation.nosql.mongo_setup as mongo_setup
 import delphin_6_automation.pytest.pytest_helper_functions as helper
 from delphin_6_automation.simulation.nosql.auth import dtu_byg
+import delphin_6_automation.simulation.nosql.db_templates.result_entry as result_db
 
 # -------------------------------------------------------------------------------------------------------------------- #
 # TEST
@@ -21,7 +23,7 @@ from delphin_6_automation.simulation.nosql.auth import dtu_byg
 mongo_setup.global_init(dtu_byg)
 
 
-def test_download_1():
+def test_download_project_1():
     id_ = "5a5479095d9460327c6970f0"
     test_path, source_path = helper.setup_test_folders()
     test_file_path = os.path.dirname(os.path.realpath(__file__)) + '/test_files'
@@ -29,3 +31,31 @@ def test_download_1():
     shutil.copy(test_file_path + '/' + id_ + '.d6p', source_path + '/' + id_ + '.d6p')
     assert filecmp.cmp(source_path + '/' + str(id_) + '.d6p', test_path + '/' + str(id_) + '.d6p')
     helper.clean_up_test_folders()
+
+
+def test_download_results_1():
+    test_path, source_path = helper.setup_test_folders()
+    id_ = '5a54a8a35d946027d804103c'
+    general_interact.download_raw_result(id_, test_path)
+
+    source_zip = os.path.dirname(os.path.realpath(__file__)) + '/test_files/5a5479095d9460327c6970f0.zip'
+    shutil.unpack_archive(source_zip, source_path)
+
+    for file in os.listdir(test_path + '/log'):
+        print(filecmp.cmp(source_path + '/5a5479095d9460327c6970f0/log/' + file, test_path + '/log/' + file))
+
+    for file in os.listdir(test_path + '/results'):
+        assert filecmp.cmp(source_path + '/5a5479095d9460327c6970f0/results/' + file, test_path + '/results/' + file)
+
+
+def test_download_materials_1():
+    # TODO
+    pass
+
+
+def test_download_weather_1():
+    # TODO
+    pass
+
+
+test_download_results_1()
