8dfd79b237abedbd37f33ba76110fc366253d5f3
ocni <ocni@dtu.dk>
2018-02-06 14:57:05 +0100

    updated test_delphin_permutations

--- delphin_6_automation/pytest/test_delphin_permutations.py
+++ delphin_6_automation/pytest/test_delphin_permutations.py
@@ -9,25 +9,26 @@ from collections import OrderedDict
 import xmltodict
 
 # RiBuild Modules:
-import delphin_6_automation.database_interactions.delphin_interactions as delphin_interact
+from delphin_6_automation.database_interactions import delphin_interactions
+from delphin_6_automation.file_parsing import delphin_parser
 import delphin_6_automation.nosql.mongo_setup as mongo_setup
 import delphin_6_automation.pytest.pytest_helper_functions as helper
 from delphin_6_automation.nosql.auth import dtu_byg
-import delphin_6_automation.delphin_setup.delphin_permutations as delphin_permu
+from delphin_6_automation.delphin_setup import delphin_permutations
 
 # -------------------------------------------------------------------------------------------------------------------- #
 # TEST
 
 mongo_setup.global_init(dtu_byg)
 
-# TODO - Update test permetuations
+# TODO - Update test permutations
 
 def test_get_layers_1():
     id_ = "5a5479095d9460327c6970f0"
     test_path, source_path = helper.setup_test_folders()
-    delphin_interact.download_from_database(id_, test_path)
-    delphin_dict = delphin_interact.dp6_to_dict(test_path + '/' + id_ + '.d6p')
-    delphin_layers = delphin_permu.get_layers(delphin_dict)
+    delphin_interactions.download_delphin_entry(id_, test_path)
+    delphin_dict = delphin_parser.dp6_to_dict(test_path + '/' + id_ + '.d6p')
+    delphin_layers = delphin_permutations.get_layers(delphin_dict)
     helper.clean_up_test_folders()
 
     correct_layer_dict = {0: {'material': 'Lime Cement Plaster light [630]',
@@ -55,13 +56,13 @@ def test_get_layers_1():
 def test_change_material_1():
     id_ = "5a5479095d9460327c6970f0"
     test_path, source_path = helper.setup_test_folders()
-    delphin_interact.download_from_database(id_, test_path)
-    delphin_dict = delphin_interact.dp6_to_dict(test_path + '/' + id_ + '.d6p')
+    delphin_interactions.download_delphin_entry(id_, test_path)
+    delphin_dict = delphin_parser.dp6_to_dict(test_path + '/' + id_ + '.d6p')
     new_material = OrderedDict((('@name', 'Aerated Concrete [6]'),
                                 ('@color', '#ff404060'),
                                 ('@hatchCode', '13'),
                                 ('#text', '${Material Database}/AeratedConcrete_6.m6')))
-    new_delphin = delphin_permu.change_layer_material(delphin_dict, 'Normal Brick [512]', new_material)
+    new_delphin = delphin_permutations.change_layer_material(delphin_dict, 'Normal Brick [512]', new_material)
     xmltodict.unparse(new_delphin, output=open(test_path + '/' + id_ + '.d6p', 'w'), pretty=True)
     helper.clean_up_test_folders()
 
@@ -69,21 +70,21 @@ def test_change_material_1():
 def test_change_layer_width():
     id_ = "5a5479095d9460327c6970f0"
     test_path, source_path = helper.setup_test_folders()
-    delphin_interact.download_from_database(id_, test_path)
-    delphin_dict = delphin_interact.dp6_to_dict(test_path + '/' + id_ + '.d6p')
-    old_width = sum(delphin_permu.convert_discretization_to_list(delphin_dict))
-    new_delphin = delphin_permu.change_layer_width(delphin_dict, 'Normal Brick [512]', 1.0)
+    delphin_interactions.download_delphin_entry(id_, test_path)
+    delphin_dict = delphin_parser.dp6_to_dict(test_path + '/' + id_ + '.d6p')
+    old_width = sum(delphin_permutations.convert_discretization_to_list(delphin_dict))
+    new_delphin = delphin_permutations.change_layer_width(delphin_dict, 'Normal Brick [512]', 1.0)
     xmltodict.unparse(new_delphin, output=open(test_path + '/' + id_ + '_test.d6p', 'w'), pretty=True)
     helper.clean_up_test_folders()
-    assert sum(delphin_permu.convert_discretization_to_list(new_delphin)) - old_width - 0.65 <= 0.002
+    assert sum(delphin_permutations.convert_discretization_to_list(new_delphin)) - old_width - 0.65 <= 0.002
 
 
 def test_change_weather():
     id_ = "5a5479095d9460327c6970f0"
     test_path, source_path = helper.setup_test_folders()
-    delphin_interact.download_from_database(id_, test_path)
-    delphin_dict = delphin_interact.dp6_to_dict(test_path + '/' + id_ + '.d6p')
-    new_delphin = delphin_permu.change_weather(delphin_dict,
+    delphin_interactions.download_delphin_entry(id_, test_path)
+    delphin_dict = delphin_parser.dp6_to_dict(test_path + '/' + id_ + '.d6p')
+    new_delphin = delphin_permutations.change_weather(delphin_dict,
                                                'Temp Out',
                                                r'C:/Program Files (x86)/IBK/Delphin 5.9/DB_climate_data/Germany/Kassel/Temperature.ccd')
     xmltodict.unparse(new_delphin, output=open(test_path + '/' + id_ + '_test.d6p', 'w'), pretty=True)
