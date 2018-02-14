6c1c1318f55dbb843d3794ea047f9f8fe16f68a1
ocni <ocni@dtu.dk>
2018-02-14 10:39:05 +0100

    updated test and related functions

--- delphin_6_automation/pytest/test_delphin_permutations.py
+++ delphin_6_automation/pytest/test_delphin_permutations.py
@@ -6,31 +6,27 @@ __author__ = "Christian Kongsgaard"
 
 # Modules:
 from collections import OrderedDict
-
+import os
 import xmltodict
-from delphin_6_automation.database_interactions.auth import dtu_byg
 
-import delphin_6_automation.database_interactions.mongo_setup as mongo_setup
-import delphin_6_automation.pytest.pytest_helper_functions as helper
 # RiBuild Modules:
+from delphin_6_automation.database_interactions.auth import dtu_byg
 from delphin_6_automation.database_interactions import delphin_interactions
 from delphin_6_automation.delphin_setup import delphin_permutations
 from delphin_6_automation.file_parsing import delphin_parser
+import delphin_6_automation.database_interactions.mongo_setup as mongo_setup
+import delphin_6_automation.pytest.pytest_helper_functions as helper
 
 # -------------------------------------------------------------------------------------------------------------------- #
 # TEST
 
 mongo_setup.global_init(dtu_byg)
 
-# TODO - Update test permutations
 
 def test_get_layers_1():
-    id_ = "5a5479095d9460327c6970f0"
-    test_path, source_path = helper.setup_test_folders()
-    delphin_interactions.download_delphin_entry(id_, test_path)
-    delphin_dict = delphin_parser.dp6_to_dict(test_path + '/' + id_ + '.d6p')
+    test_path = r'C:\Users\ocni\PycharmProjects\delphin_6_automation\delphin_6_automation\pytest\test_files'
+    delphin_dict = delphin_parser.dp6_to_dict(test_path + '/delphin_project.d6p')
     delphin_layers = delphin_permutations.get_layers(delphin_dict)
-    helper.clean_up_test_folders()
 
     correct_layer_dict = {0: {'material': 'Lime Cement Plaster light [630]',
                               'x_width': 0.009999999999999998,
@@ -55,38 +51,42 @@ def test_get_layers_1():
 
 
 def test_change_material_1():
-    id_ = "5a5479095d9460327c6970f0"
-    test_path, source_path = helper.setup_test_folders()
-    delphin_interactions.download_delphin_entry(id_, test_path)
-    delphin_dict = delphin_parser.dp6_to_dict(test_path + '/' + id_ + '.d6p')
+    source_path = r'C:\Users\ocni\PycharmProjects\delphin_6_automation\delphin_6_automation\pytest\test_files'
+    test_path, _ = helper.setup_test_folders()
+    delphin_dict = delphin_parser.dp6_to_dict(source_path + '/delphin_project.d6p')
     new_material = OrderedDict((('@name', 'Aerated Concrete [6]'),
                                 ('@color', '#ff404060'),
                                 ('@hatchCode', '13'),
                                 ('#text', '${Material Database}/AeratedConcrete_6.m6')))
     new_delphin = delphin_permutations.change_layer_material(delphin_dict, 'Normal Brick [512]', new_material)
-    xmltodict.unparse(new_delphin, output=open(test_path + '/' + id_ + '.d6p', 'w'), pretty=True)
+    xmltodict.unparse(new_delphin, output=open(test_path + '/modified_delphin_project.d6p', 'w'), pretty=True)
     helper.clean_up_test_folders()
+
+    assert new_delphin['DelphinProject']['Materials']['MaterialReference'][1] == new_material
 
 
 def test_change_layer_width():
-    id_ = "5a5479095d9460327c6970f0"
-    test_path, source_path = helper.setup_test_folders()
-    delphin_interactions.download_delphin_entry(id_, test_path)
-    delphin_dict = delphin_parser.dp6_to_dict(test_path + '/' + id_ + '.d6p')
+    source_path = r'C:\Users\ocni\PycharmProjects\delphin_6_automation\delphin_6_automation\pytest\test_files'
+    test_path, _ = helper.setup_test_folders()
+    delphin_dict = delphin_parser.dp6_to_dict(source_path + '/delphin_project.d6p')
     old_width = sum(delphin_permutations.convert_discretization_to_list(delphin_dict))
+
     new_delphin = delphin_permutations.change_layer_width(delphin_dict, 'Normal Brick [512]', 1.0)
-    xmltodict.unparse(new_delphin, output=open(test_path + '/' + id_ + '_test.d6p', 'w'), pretty=True)
+    xmltodict.unparse(new_delphin, output=open(test_path + '/modified_delphin_project.d6p', 'w'), pretty=True)
     helper.clean_up_test_folders()
+
     assert sum(delphin_permutations.convert_discretization_to_list(new_delphin)) - old_width - 0.65 <= 0.002
 
 
 def test_change_weather():
-    id_ = "5a5479095d9460327c6970f0"
-    test_path, source_path = helper.setup_test_folders()
-    delphin_interactions.download_delphin_entry(id_, test_path)
-    delphin_dict = delphin_parser.dp6_to_dict(test_path + '/' + id_ + '.d6p')
+    source_path = r'C:\Users\ocni\PycharmProjects\delphin_6_automation\delphin_6_automation\pytest\test_files'
+    test_path, _ = helper.setup_test_folders()
+    delphin_dict = delphin_parser.dp6_to_dict(source_path + '/delphin_project.d6p')
+    weather_path = os.path.dirname(os.path.realpath(__file__)) + '/test_files/temperature.ccd'
     new_delphin = delphin_permutations.change_weather(delphin_dict,
-                                               'Temp Out',
-                                               r'C:/Program Files (x86)/IBK/Delphin 5.9/DB_climate_data/Germany/Kassel/Temperature.ccd')
-    xmltodict.unparse(new_delphin, output=open(test_path + '/' + id_ + '_test.d6p', 'w'), pretty=True)
+                                                      'Temp Out',
+                                                      weather_path)
+    xmltodict.unparse(new_delphin, output=open(test_path + '/modified_delphin_project.d6p', 'w'), pretty=True)
     helper.clean_up_test_folders()
+
+    assert new_delphin['DelphinProject']['Conditions']['ClimateConditions']['ClimateCondition'][2]['Filename'] == weather_path
