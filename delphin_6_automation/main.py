9cddc996a3d7b3a50e793a6cd4b75147a3a0d7fb
ocni <ocni@dtu.dk>
2018-03-27 12:51:09 +0200

    update to logging

--- delphin_6_automation/main.py
+++ delphin_6_automation/main.py
@@ -7,13 +7,14 @@ __license__ = 'MIT'
 # Modules:
 import os
 import sys
-from delphin_6_automation.ribuild_logger import logger
+
 
 source_folder = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 sys.path.insert(0, source_folder)
 
 # RiBuild Modules:
-from delphin_6_automation.backend import *
+from delphin_6_automation.ribuild_logger import logger
+from delphin_6_automation.backend import main
 
 # -------------------------------------------------------------------------------------------------------------------- #
 # BACKEND
