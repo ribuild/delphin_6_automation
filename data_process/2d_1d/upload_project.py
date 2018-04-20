36a5c707eee75b9174e68de12f45bd7fcef38720
ocni <ocni@dtu.dk>
2018-04-20 14:29:35 +0200

    minor update to data_process

--- data_process/2d_1d/upload_project.py
+++ data_process/2d_1d/upload_project.py
@@ -30,18 +30,38 @@ climate_class = 'a'
 location_name = 'KobenhavnTaastrup'
 years = [2020, 2020, 2021, 2022]
 
-sim_id = general_interactions.add_to_simulation_queue(delphin_file5, priority)
+sim_id = general_interactions.add_to_simulation_queue(delphin_file0, priority)
 weather_interactions.assign_indoor_climate_to_project(sim_id, climate_class)
 weather_interactions.assign_weather_by_name_and_years(sim_id, location_name, years)
 delphin_interactions.change_entry_simulation_length(sim_id, len(years), 'a')
 
 
-materials = [492, 542]
-layer_material = 'Old Building Brick Dresden ZP [504]'
-priority_ = 1
-modified_ids = delphin_interactions.permutate_entry_layer_material(sim_id, layer_material, materials, priority_)
+def permutate_uploads():
+    materials = [492, 542, 148]
+    layer_material = 'Old Building Brick Dresden ZP [504]'
+    second_layer = 'Lime cement mortar [717]'
+    second_material = [718]
+    priority_ = 1
+    modified_ids = delphin_interactions.permutate_entry_layer_material(sim_id, layer_material, materials, priority_)
 
-for modified_id in modified_ids:
-    weather_interactions.assign_weather_by_name_and_years(modified_id, location_name, years)
-    weather_interactions.assign_indoor_climate_to_project(modified_id, climate_class)
-    delphin_interactions.change_entry_simulation_length(modified_id, len(years), 'a')
+    for modified_id in modified_ids:
+        weather_interactions.assign_weather_by_name_and_years(modified_id, location_name, years)
+        weather_interactions.assign_indoor_climate_to_project(modified_id, climate_class)
+        delphin_interactions.change_entry_simulation_length(modified_id, len(years), 'a')
+
+        # Second Layer
+        second_id = delphin_interactions.permutate_entry_layer_material(modified_id, second_layer,
+                                                                        second_material, priority_)[0]
+        weather_interactions.assign_weather_by_name_and_years(second_id, location_name, years)
+        weather_interactions.assign_indoor_climate_to_project(second_id, climate_class)
+        delphin_interactions.change_entry_simulation_length(second_id, len(years), 'a')
+
+    # Catch first sim
+    third_id = delphin_interactions.permutate_entry_layer_material(sim_id, second_layer,
+                                                                   second_material, priority_)[0]
+    weather_interactions.assign_weather_by_name_and_years(third_id, location_name, years)
+    weather_interactions.assign_indoor_climate_to_project(third_id, climate_class)
+    delphin_interactions.change_entry_simulation_length(third_id, len(years), 'a')
+
+
+permutate_uploads()
