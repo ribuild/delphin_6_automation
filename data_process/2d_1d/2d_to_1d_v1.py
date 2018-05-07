83f31d36f1e3b21ae78188c8d70defcf45991c4e
ocni <ocni@dtu.dk>
2018-05-07 12:52:15 +0200

    updated data process - updated plot functions

--- data_process/2d_1d/2d_to_1d_v1.py
+++ data_process/2d_1d/2d_to_1d_v1.py
@@ -16,29 +16,63 @@ from scipy.stats import linregress
 # -------------------------------------------------------------------------------------------------------------------- #
 # RIBuild
 
-acronym_file = r'U:\RIBuild\2D_1D\2D to 1D Transformation_tmp.xlsx'
+acronym_file = r'U:\RIBuild\2D_1D\2D to 1D Transformation.xlsx'
 out_folder = r'C:\Users\ocni\PycharmProjects\delphin_6_automation\data_process\2d_1d\processed_data'
 result_folder = r'U:\RIBuild\2D_1D\Results'
-# dp.process_results(acronym_file, result_folder, out_folder)
+graphic_folder = r'U:\RIBuild\2D_1D\Processed Results\4A'
+#dp.process_results(acronym_file, result_folder, out_folder)
 
-hdf_file = out_folder + '/moisture content.h5'
+quantities = ['heat loss', 'temperature', 'relative humidity', 'moisture content', 'moisture integral']
+quantity = quantities[4]
+hdf_file = out_folder + '/' + quantity + '.h5'
+
 
 # Open HDF
-# Uninsulated
-dresdenzp_high_uninsulated_4a = pd.read_hdf(hdf_file, 'dresden_zp_high_cement_uninsulated_36_4a')
-dresdenzd_high_uninsulated_4a = pd.read_hdf(hdf_file, 'dresden_zd_high_cement_uninsulated_36_4a')
-postdam_high_uninsulated_4a = pd.read_hdf(hdf_file, 'potsdam_high_cement_uninsulated_36_4a')
-dresdenzp_low_uninsulated_4a = pd.read_hdf(hdf_file, 'dresden_zp_low_cement_uninsulated_36_4a')
-dresdenzd_low_uninsulated_4a = pd.read_hdf(hdf_file, 'dresden_zd_low_cement_uninsulated_36_4a')
-postdam_low_uninsulated_4a = pd.read_hdf(hdf_file, 'potsdam_low_cement_uninsulated_36_4a')
-
-total_uninsulated_4a = pd.concat([dresdenzp_high_uninsulated_4a, dresdenzd_high_uninsulated_4a,
-                                  postdam_high_uninsulated_4a, dresdenzp_low_uninsulated_4a,
-                                  dresdenzd_low_uninsulated_4a, postdam_low_uninsulated_4a])
+total_uninsulated_4a = pd.read_hdf(hdf_file, 'total_4a_36_uninsulated')
+total_insulated_4a = pd.read_hdf(hdf_file, 'total_4a_36_insulated')
 
 
-#dp.plot_linear_relation(total_uninsulated_4a, 'mortar', (0, 225))
-dp.abs_diff_boxplot(total_uninsulated_4a, (-50, 50))
+def uninsulated(save=False):
 
+    dp.abs_diff_boxplot(total_uninsulated_4a, (-2, 2), quantity.title(), '4A 36cm Uninsulated')
+    if save:
+        plt.savefig(f'{graphic_folder}/{quantity}_abs_diff_uninsulated.png')
+
+    dp.rel_diff_boxplot(total_uninsulated_4a, (0, 250), quantity.title(), '4A 36cm Uninsulated', log=False)
+    if save:
+        plt.savefig(f'{graphic_folder}/{quantity}_rel_diff_uninsulated.png')
+
+    dp.plot_linear_relation(total_uninsulated_4a, 'mortar', (0, 3), quantity.title(), '4A 36cm Uninsulated')
+    if save:
+        plt.savefig(f'{graphic_folder}/{quantity}_linear_relation_mortar_uninsulated.png')
+
+    dp.plot_linear_relation(total_uninsulated_4a, 'brick', (0, 3), quantity.title(), '4A 36cm Uninsulated')
+    if save:
+        plt.savefig(f'{graphic_folder}/{quantity}_linear_relation_brick_uninsulated.png')
+
+
+uninsulated(True)
+
+
+def insulated(save=False):
+
+    dp.abs_diff_boxplot(total_insulated_4a, (-5, 5), quantity.title(), '4A 36cm Insulated')
+    if save:
+        plt.savefig(f'{graphic_folder}/{quantity}_abs_diff_insulated.png')
+
+    dp.rel_diff_boxplot(total_insulated_4a, (0.1, 120), quantity.title(), '4A 36cm Insulated', log=False)
+    if save:
+        plt.savefig(f'{graphic_folder}/{quantity}_rel_diff_insulated.png')
+
+    dp.plot_linear_relation(total_insulated_4a, 'mortar', (0, 5), quantity.title(), '4A 36cm Insulated')
+    if save:
+        plt.savefig(f'{graphic_folder}/{quantity}_linear_relation_mortar_insulated.png')
+
+    dp.plot_linear_relation(total_insulated_4a, 'brick', (0, 5), quantity.title(), '4A 36cm Insulated')
+    if save:
+        plt.savefig(f'{graphic_folder}/{quantity}_linear_relation_brick_insulated.png')
+
+
+#insulated(True)
 
 plt.show()
--- data_process/2d_1d/data_process_functions.py
+++ data_process/2d_1d/data_process_functions.py
@@ -134,35 +134,126 @@ def process_results(excel_file, result_folder, out_folder):
             new_table = compute_differences(new_table)
             save_to_hdf(new_table, acronym, quantity, out_folder)
 
+        create_totals(out_folder, quantity)
 
-def plot_linear_relation(data_frame, material, bounds):
+
+def create_totals(out_folder, quantity):
+
+    hdf_file = out_folder + '/' + quantity + '.h5'
+    store = pd.HDFStore(hdf_file)
+
+    store_names = {'1a': {'48': {'insulated': [], 'uninsulated': []},
+                          '36': {'insulated': [], 'uninsulated': []},
+                          '24': {'insulated': [], 'uninsulated': []}
+                          },
+                   '2a': {'48': {'insulated': [], 'uninsulated': []},
+                          '36': {'insulated': [], 'uninsulated': []},
+                          '24': {'insulated': [], 'uninsulated': []}
+                          },
+                   '3a': {'48': {'insulated': [], 'uninsulated': []},
+                          '36': {'insulated': [], 'uninsulated': []},
+                          '24': {'insulated': [], 'uninsulated': []}
+                          },
+                   '4a': {'48': {'insulated': [], 'uninsulated': []},
+                          '36': {'insulated': [], 'uninsulated': []},
+                          '24': {'insulated': [], 'uninsulated': []}
+                          }}
+
+    for group in store.groups():
+        group_name = group._v_name
+
+        if '_4a' in group_name:
+            if '_48_' in group_name:
+                if '_insulated_' in group_name:
+                    store_names['4a']['48']['insulated'].append(group_name)
+                else:
+                    store_names['4a']['48']['uninsulated'].append(group_name)
+            elif '_36_' in group_name:
+                if '_insulated_' in group_name:
+                    store_names['4a']['36']['insulated'].append(group_name)
+                else:
+                    store_names['4a']['36']['uninsulated'].append(group_name)
+            elif '_24_' in group_name:
+                if '_insulated_' in group_name:
+                    store_names['4a']['24']['insulated'].append(group_name)
+                else:
+                    store_names['4a']['24']['uninsulated'].append(group_name)
+
+    for key0 in store_names.keys():
+        for key1 in store_names[key0].keys():
+            for key2 in store_names[key0][key1].keys():
+
+                if store_names[key0][key1][key2]:
+                    frame_list = []
+                    for frame in store_names[key0][key1][key2]:
+                        frame_list.append(store.select(frame))
+
+                    store.append(value=pd.concat(frame_list), key=f'total_{key0}_{key1}_{key2}')
+
+    store.close()
+
+
+def plot_linear_relation(data_frame, material, bounds, quantity, title):
+    quantities = ['heat loss', 'temperature', 'relative humidity', 'moisture content', 'moisture integral']
+    units = ['W/m$^2$', '$^o$C', '%', 'kg/m$^3$', 'kg']
+    i = quantities.index(quantity.lower())
+
+    if quantity.lower() == 'heat loss' or quantity.lower() == 'moisture integral':
+        fig, axes = plt.subplots(figsize=(16, 8), )
+        fig.suptitle(f'{title} - {quantity}\n{material.title()}')
+
+    elif 'Uninsulated' in title:
+        fig, axes = plt.subplots(ncols=3, nrows=2, sharex=True, sharey=True, figsize=(16, 8), )
+        fig.suptitle(f'{title} - {quantity}\n{material.title()}')
+        axes = axes.flatten()
+    else:
+        fig, axes = plt.subplots(ncols=4, nrows=2, sharex=True, sharey=True, figsize=(16, 8), )
+        fig.suptitle(f'{title} - {quantity}\n{material.title()}')
+        axes = axes.flatten()
 
     for location in data_frame.columns.levels[0]:
-        plt.figure()
-        plt.title(f'Location {location}, {material}')
-        slope, intercept, r_value, p_value, std_err = linregress(data_frame[location, '2d', 'out'],
-                                                                 data_frame[location, material, 'out'])
-        plt.scatter(data_frame[location, '2d', 'out'], data_frame[location, material, 'out'], label='Data')
-        plt.plot(data_frame[location, '2d', 'out'], intercept + slope * data_frame[location, '2d', 'out'], 'r',
-                 label=f'f(x) = {slope:.4f} * x + {intercept:.4f}\nR$^2$ = {r_value:.2f}')
-        plt.ylim(bounds[0], bounds[1])
-        plt.xlim(bounds[0], bounds[1])
-        plt.legend()
+        if quantity.lower() == 'heat loss' or quantity.lower() == 'moisture integral':
+            ax = axes
+        else:
+            ax = axes[int(location)]
+            ax.set_title(f'Location {location}')
+
+        slope, intercept, r_value, p_value, std_err = linregress(data_frame[location, material, 'out'],
+                                                                 data_frame[location, '2d', 'out'])
+        ax.scatter(data_frame[location, material, 'out'], data_frame[location, '2d', 'out'], label='Data')
+        ax.plot(data_frame[location, material, 'out'], intercept + slope * data_frame[location, material, 'out'], 'r',
+                label=f'f(x) = {slope:.4f} * x + {intercept:.4f}\nR$^2$ = {r_value:.2f}')
+        ax.set_ylim(bounds[0], bounds[1])
+        ax.set_xlim(bounds[0], bounds[1])
+        ax.set_ylabel(f'2D Result - {units[i]}')
+        ax.set_xlabel(f'1D Result - {units[i]}')
+        ax.legend()
 
 
-def abs_diff_boxplot(data_frame, bounds):
-
+def abs_diff_boxplot(data_frame, bounds, quantity, title):
+    quantities = ['heat loss', 'temperature', 'relative humidity', 'moisture content', 'moisture integral']
+    units = ['W/m$^2$', '$^o$C', '%', 'kg/m$^3$', 'kg']
+    i = quantities.index(quantity.lower())
     abs_frame = data_frame.loc[:, pd.IndexSlice[:, :, 'abs_diff']]
     abs_frame.columns = abs_frame.columns.droplevel(level=2)
-    plt.figure()
+    plt.figure(figsize=(16, 8), tight_layout=True)
+    plt.title(f'Absolute Difference - {quantity}\n'
+              f'{title}')
     abs_frame.boxplot(showfliers=False)
     plt.ylim(bounds[0], bounds[1])
+    plt.ylabel(f'{quantity} - {units[i]}')
 
 
-def rel_diff_boxplot(data_frame, bounds):
+def rel_diff_boxplot(data_frame, bounds, quantity, title, log=False):
 
     rel_frame = data_frame.loc[:, pd.IndexSlice[:, :, 'rel_diff']]
     rel_frame.columns = rel_frame.columns.droplevel(level=2)
-    plt.figure()
+    plt.figure(figsize=(16, 8), tight_layout=True)
+    plt.title(f'Relative Difference - {quantity}\n'
+              f'{title}')
     rel_frame.boxplot(showfliers=False)
     plt.ylim(bounds[0], bounds[1])
+    plt.ylabel(f'Relative Difference - %')
+
+    if log:
+        plt.gca().set_yscale('log')
