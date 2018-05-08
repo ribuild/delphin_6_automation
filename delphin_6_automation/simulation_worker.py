27cddca8ba39389568805bb45b91e6c52a6d2b3e
ocni <ocni@dtu.dk>
2018-05-08 16:15:36 +0200

    removed unused imports

--- delphin_6_automation/simulation_worker.py
+++ delphin_6_automation/simulation_worker.py
@@ -17,11 +17,9 @@ import paramiko
 
 # RiBuild Modules:
 from delphin_6_automation.database_interactions.db_templates import delphin_entry
-from delphin_6_automation.logging.ribuild_logger import ribuild_logger, notifiers_logger
 from delphin_6_automation.database_interactions import simulation_interactions
 from delphin_6_automation.database_interactions import delphin_interactions
 from delphin_6_automation.database_interactions import general_interactions
-import delphin_6_automation.database_interactions.mongo_setup as mongo_setup
 import delphin_6_automation.database_interactions.db_templates.result_raw_entry as result_db
 from delphin_6_automation.database_interactions.auth import hpc
 
