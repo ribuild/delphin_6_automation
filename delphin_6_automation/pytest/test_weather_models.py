61c7f8642477917703cc871202b83dbdbdcf5f64
ocni <ocni@dtu.dk>
2018-06-27 11:30:43 +0200

    updated weather model tests

--- delphin_6_automation/pytest/test_weather_models.py
+++ delphin_6_automation/pytest/test_weather_models.py
@@ -7,7 +7,6 @@ __license__ = 'MIT'
 # Modules
 import os
 import numpy as np
-import matplotlib.pyplot as plt
 
 # RiBuild Modules
 from delphin_6_automation.delphin_setup import weather_modeling
@@ -17,28 +16,20 @@ from delphin_6_automation.file_parsing import weather_parser
 # TEST WEATHER MODELS
 
 
-def test_rain_model():
-    folder = os.path.join(os.path.dirname(__file__), 'test_files')
+def test_rain_model(test_folder):
 
-    rain = weather_parser.ccd_to_list(folder + '/vertical_rain.ccd')
-    wind_speed = weather_parser.ccd_to_list(folder + '/wind_speed.ccd')
-    wind_direction = weather_parser.ccd_to_list(folder + '/wind_direction.ccd')
+    rain = weather_parser.ccd_to_list(test_folder + '/weather/vertical_rain.ccd')
+    wind_speed = weather_parser.ccd_to_list(test_folder + '/weather/wind_speed.ccd')
+    wind_direction = weather_parser.ccd_to_list(test_folder + '/weather/wind_direction.ccd')
     wall_location = {'height': 5, 'width': 5}
     wdr = weather_modeling.driving_rain(rain, wind_direction, wind_speed, wall_location, 90, 0)
 
     assert rain == wdr
 
 
-def test_solar_radiation():
-    folder = os.path.join(os.path.dirname(__file__), 'test_files')
+def test_solar_radiation(test_folder):
 
-    diff_rad = np.array(weather_parser.ccd_to_list(folder + '/diffuse_radiation.ccd'))
-    dir_rad = np.array(weather_parser.ccd_to_list(folder + '/direct_radiation.ccd'))
+    diff_rad = np.array(weather_parser.ccd_to_list(test_folder + '/weather/diffuse_radiation.ccd'))
+    dir_rad = np.array(weather_parser.ccd_to_list(test_folder + '/weather/direct_radiation.ccd'))
     radiation = diff_rad + dir_rad
     short_wave = weather_modeling.short_wave_radiation(radiation, -2.083, 57.167, 0, 230)
-
-    plt.figure()
-    plt.plot(np.linspace(0, len(radiation), len(radiation)), short_wave)
-    plt.show()
-
-    print(short_wave)
