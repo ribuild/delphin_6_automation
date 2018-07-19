295dcfb75b4ed1fa8b34b99e9e8c12ac0385ef1c
ocni <ocni@dtu.dk>
2018-07-19 11:08:24 +0200

    added test for get_average__computation_time

--- delphin_6_automation/pytest/test_simulation_worker.py
+++ delphin_6_automation/pytest/test_simulation_worker.py
@@ -5,10 +5,13 @@ __license__ = 'MIT'
 # IMPORTS
 
 # Modules
-
+import pytest
+import datetime
 
 # RiBuild Modules
 from delphin_6_automation.backend import simulation_worker
+from delphin_6_automation.database_interactions.db_templates import delphin_entry
+from delphin_6_automation.database_interactions import simulation_interactions
 
 # -------------------------------------------------------------------------------------------------------------------- #
 # RIBuild
@@ -30,9 +33,21 @@ def test_wait_until_finished():
 def test_hpc_worker():
     assert True
 
+@pytest.mark.parametrize('sim_time',
+                         [False, True])
+def test_get_average_computation_time(db_one_project, sim_time):
 
-def test_get_average_computation_time():
+    delphin_id = delphin_entry.Delphin.objects().first().id
+    if sim_time:
+        delta_time = datetime.timedelta(minutes=3)
+        simulation_interactions.set_simulation_time(delphin_id, delta_time)
 
     computation_time = simulation_worker.get_average_computation_time(delphin_id)
 
-    assert True
+    assert computation_time
+    assert isinstance(computation_time, int)
+
+    if sim_time:
+        assert computation_time == 3
+    else:
+        assert computation_time == 15
