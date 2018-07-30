0b18d4af529394dc287cd32d5417483338c3a915
ocni <ocni@dtu.dk>
2018-07-30 12:24:32 +0200

    bug fix

---
+++ delphin_6_automation/sampling_scheme.py
@@ -0,0 +1,27 @@
+__author__ = "Christian Kongsgaard"
+__license__ = 'MIT'
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# IMPORTS
+
+# Modules
+import os
+import sys
+
+source_folder = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+sys.path.insert(0, source_folder)
+
+# RiBuild Modules
+from delphin_6_automation.database_interactions import mongo_setup
+from delphin_6_automation.database_interactions.auth import auth_dict
+from delphin_6_automation.backend.sampling_worker import main
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# RIBuild
+
+
+if __name__ == "__main__":
+    # Setup connection
+    mongo_setup.global_init(auth_dict)
+
+    main()
