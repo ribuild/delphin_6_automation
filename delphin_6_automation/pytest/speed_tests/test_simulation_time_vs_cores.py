e7573093f90641502b22291bf1584c4974248444
ocni <ocni@dtu.dk>
2018-08-07 14:23:39 +0200

    created speed test folder

--- delphin_6_automation/pytest/speed_tests/test_simulation_time_vs_cores.py
+++ delphin_6_automation/pytest/speed_tests/test_simulation_time_vs_cores.py
@@ -19,6 +19,10 @@ from delphin_6_automation.delphin_setup import weather_modeling
 # -------------------------------------------------------------------------------------------------------------------- #
 # RIBuild
 
+#
+# Result: 6 cores is the fastest for HPC simulation
+#
+
 
 @pytest.fixture(params=[2, 4, 6, 8, 12, 16, 20, 24])
 def mock_submit_file(monkeypatch, request):
@@ -57,35 +61,6 @@ def mock_submit_file(monkeypatch, request):
         return submit_file, computation_time
 
     monkeypatch.setattr(simulation_worker, 'create_submit_file', mock_return)
-
-
-@pytest.fixture()
-def mock_wait_until_finished(monkeypatch):
-
-    def mock_return(sim_id: str, estimated_run_time: int, simulation_folder: str):
-        finished = False
-        start_time = None
-
-        while not start_time:
-            if os.path.exists(f"{simulation_folder}/{sim_id}"):
-                start_time = datetime.datetime.now()
-
-        while not finished:
-
-            if os.path.exists(f"{simulation_folder}/{sim_id}/log/summary.txt"):
-                finished = True
-
-    monkeypatch.setattr(simulation_worker, 'wait_until_finished', mock_return)
-
-
-@pytest.fixture()
-def mock_driving_rain(monkeypatch):
-
-    def mock_return(precipitation: list, wind_direction: list, wind_speed: list, wall_location: dict,
-                 orientation, inclination=90, catch_ratio=None):
-        return [0.0 for _ in range(len(precipitation))]
-
-    monkeypatch.setattr(weather_modeling, 'driving_rain', mock_return)
 
 
 @pytest.mark.skipif(platform.system() == 'Linux', reason='Test should only run locally')
