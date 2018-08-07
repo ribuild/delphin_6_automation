bf02cd070974ac9947474cb1cfcd22c313adbcea
ocni <ocni@dtu.dk>
2018-08-07 13:40:11 +0200

    bug fix to simulation speed test

--- delphin_6_automation/pytest/test_simulation_speed_vs_cores.py
+++ delphin_6_automation/pytest/test_simulation_speed_vs_cores.py
@@ -20,7 +20,7 @@ from delphin_6_automation.delphin_setup import weather_modeling
 # RIBuild
 
 
-@pytest.fixture(params=[2, 4, 6, 8, 12, 16, ])
+@pytest.fixture(params=[2, 4, 6, 8, 12, 16, 20, 24])
 def mock_submit_file(monkeypatch, request):
 
     def mock_return(sim_id: str, simulation_folder: str, restart=False):
