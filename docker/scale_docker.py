752cd7706d2f64a535bbd10e2af800959a177b7f
Christian Kongsgaard <ocni@byg-g1272.win.dtu.dk>
2019-07-11 13:36:26 +0000

    updated scale_docker

--- docker/scale_docker.py
+++ docker/scale_docker.py
@@ -1,10 +1,16 @@
 import subprocess
+import time
 
-n = 140 
-i = 90
-while i < n:
-    process = subprocess.Popen(['docker', 'service', 'scale', f'wp6_simulation_ocni={i}'])
-    process.wait()
-    i += 10
+ret = subprocess.Popen(['docker', 'service', 'inspect', 'wp6_simulation_ocni', "-f '{{ .Spec.Mode.Replicated.Replicas }}'"], stdout=subprocess.PIPE)
+i = int(ret.stdout.readline().decode("utf-8").strip(" \n'"))
+print(f'\nCurrently there is {i} containers running')
 
-print('Done')
+
+if __name__ == "__main__":
+    n = int(input('Docker Containers: '))
+    while i < n:
+        i += 10
+        process = subprocess.Popen(['docker', 'service', 'scale', f'wp6_simulation_ocni={i}'])
+        process.wait()
+        time.sleep(10)
+    print('Done')
